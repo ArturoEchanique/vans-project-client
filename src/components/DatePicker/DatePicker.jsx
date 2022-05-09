783e599bc958d92d5c133516536510f3e11035a8
Gabriela <s.gabriela.bv96@gmail.com>
2022-05-09 18:05:49 +0200

    empiezo el date picker

---
+++ src/components/DatePicker/DatePicker.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+import Calendar from "react-calendar";
+
+
+const DatePicker = () => {
+    return (
+       
+            <Calendar />
+        
+    );
+};
+
+export default DatePicker;
