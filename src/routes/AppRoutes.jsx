b2dfe84e460ea862d279a0ca155463befb59c77a
Pablo Andreu <99558817+pandma@users.noreply.github.com>
2022-05-10 12:19:47 +0200

    login and sigup

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,15 @@
-import SignupPage from '../pages/SingupPage/SingupPage'
+import SignupPage from '../pages/SingupPage/SingupPage';
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import ResultsPage from "../pages/ResultsPage/ResultsPage";
+import LoginPage from '../pages/loginPage/loginPage';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/singup" element={SignupPage} />
+            <Route path="/singup" element={<SignupPage />} />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/results" element={<ResultsPage />} />
         </Routes>
     );
