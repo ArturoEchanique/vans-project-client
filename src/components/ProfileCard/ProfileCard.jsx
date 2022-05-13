e8802e726f337b0d80312297371af46633c20da1
Gabriela <s.gabriela.bv96@gmail.com>
2022-05-13 13:21:30 +0200

    merge

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,18 +1,26 @@
-
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth.context";
 
 const ProfileCard = () => {
+    const { user, logOutUser, isLoggedIn } = useContext(AuthContext);
 
     return (
         <div>
-            <h1>work in proccess</h1>
-            <img src="https://media2.giphy.com/media/1XgIXQEzBu6ZWappVu/giphy.gif" alt="" srcset="" />
-            <div>Profile info</div>
-            <div>Active Bookings</div>
-            <div>Past Bookinks</div>
-            <div>My Vans</div>
+            <h1>{user.username}</h1>
+            <img src={user.imageUrl} />
+            <hr />
+
+            <div>
+                <h3>activ bookings</h3>
+                <section>{user.bookings}</section>
+            </div>
+
+            <div>
+                <h3>my vans</h3>
+                <section>{user.vans}</section>
+            </div>
         </div>
+    );
+};
 
-    )
-}
-
-export default ProfileCard
+export default ProfileCard;
