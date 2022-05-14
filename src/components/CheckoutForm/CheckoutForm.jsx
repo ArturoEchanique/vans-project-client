17530434b83a641c7f225711c002e0719ac0599d
Pablo Andreu <99558817+pandma@users.noreply.github.com>
2022-05-14 20:06:26 +0200

    bookings

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -1,39 +1,52 @@
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import stripeService from "../../services/stripe.service";
 import bookingsService from "../../services/bookings.service";
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth.context";
+import userService from "../../services/user.service";
+
+
+
 
 const CheckoutForm = ({ startDate, endDate, price, van_id }) => {
-    console.log({ startDate, endDate, price, van_id })
+
+
     const stripe = useStripe()
     const elements = useElements()
     const [loading, setLoading] = useState(false);
+    const { user } = useContext(AuthContext);
+
 
     const navigate = useNavigate()
     const handleSubmit = async (e) => {
-        e.preventDefault();
+        e.preventDefault()
 
         const { error, paymentMethod } = await stripe.createPaymentMethod({
             type: "card",
             card: elements.getElement(CardElement),
         })
         setLoading(true)
-        // amount in cents
 
         if (!error) {
             const { id } = paymentMethod
-            const { data } = await stripeService.checkout({ id, amount: price * 100 })
-            console.log(data)
-        }
-        const { saveBooking } = await bookingsService.saveBooking({ startDate, endDate, price, van_id })
-        console.log(saveBooking)
+            const { data } = await stripeService.checkout({ id, amount: price * 100 }) // amount in cents
 
-        elements.getElement(CardElement).clear();
+        }
+
+        const booking = await bookingsService.saveBooking({ startDate, endDate, price, van_id })
+
+        const ownerBookings = booking.data._id
+
+        const getUser = await userService.addUserBookings(user._id, ownerBookings)
+
+        elements.getElement(CardElement).clear()
         navigate('/paydetails')
-        setLoading(false);
+        setLoading(false)
     }
-    console.log(!stripe || loading);
+
+    console.log(!stripe || loading)
 
     return (
         <form className="card card-body" onSubmit={handleSubmit}>
