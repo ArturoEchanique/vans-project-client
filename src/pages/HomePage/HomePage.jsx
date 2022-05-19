9acba3cc7b33c6d0b84ce4d4bd9ca968fc576f8e
Gabriela <s.gabriela.bv96@gmail.com>
2022-05-19 15:32:03 +0200

    merme

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react"
+import { useContext } from "react"
 import Geocode from "react-geocode"
 import { Link } from "react-router-dom"
-import { Container, Button, Row, Col } from "react-bootstrap"
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../../context/auth.context"
 import DatePicker from "../../components/DatePicker/DatePicker"
+import { Container, Button, Row, Col, Card } from "react-bootstrap"
 import "./HomePage.css"
 
 const HomePage = ({ setFilterInfo, filterData }) => {
+    const { user, logOutUser } = useContext(AuthContext)
+
     const [geoData, setGeoData] = useState({
         address: filterData.address,
     })
@@ -50,56 +54,73 @@ const HomePage = ({ setFilterInfo, filterData }) => {
     const { address, mapInitLocationX, mapInitLocationY } = filterData
 
     return (
-        <section className="home-background d-flex  align-items-center">
-            <Container>
-                <Row className="mb-3">
-                    <Col className="d-flex justify-content-center">
-                        <h1>VAN ME UP!</h1>
-                    </Col>
-                </Row>
-                <Row className="mb-3">
-                    <Col className="d-flex justify-content-center">
-                        <h3>Choose your Dates</h3>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="d-flex justify-content-center">
-                        <>
-                            <DatePicker startDate={today} endDate={tomorrow} handleDatesChange={setFilterInfo} />
-                        </>
-                        <>
-                            <Button variant="dark">
-                                <Link to="/results">Search Vans</Link>
-                            </Button>
-                        </>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="d-flex justify-content-center ">
-                        <Link to="/singup" className="nav-link logo-img">
-                            <img id="logo" src="./../images/signin.png" alt="" srcSet="" />
-                            <h3>Sing up</h3>
-                        </Link>
-                    </Col>
-                    <Col className="d-flex justify-content-center">
-                        <Link to="/login" className="nav-link  logo-img">
-                            <img id="logo" src="./../images/login.png" alt="" srcSet="" />
-                            Login
-                        </Link>
-                    </Col>
-                </Row>
-                <Row>
-                    <hr />
-                    <form>
-                        <label>
-                            Search address
-                            <input type="text" value={address} name="country" onChange={handleStreetChange} />
-                        </label>
-                        {/* <input type="submit" value="Submit" /> */}
-                    </form>
-                </Row>
-            </Container>
-        </section>
+        <>
+            <section className="home-background nav-margin">
+                <Container className="">
+                    {/* d-flex flex-column justify-content-center */}
+                    <Row className="mb-3">
+                        <Col className="">
+                            <h1 className="title">VAN ME UP!</h1>
+                        </Col>
+                    </Row>
+                    <Row className="background-row">
+                        <Row className="mb-3 ">
+                            <Col className="d-flex justify-content-center">
+                                <h3>Choose where and when</h3>
+                            </Col>
+                        </Row>
+                        <Col className="d-flex justify-content-center">
+                            <>
+                                <form>
+                                    <label>
+                                        <input className="imput-location" type="text" value={address} name="country" onChange={handleStreetChange} />
+                                    </label>
+                                </form>
+                            </>
+                            <>
+                                <DatePicker startDate={today} endDate={tomorrow} handleDatesChange={setFilterInfo} />
+                            </>
+                            <>
+                                <Button variant="dark">
+                                    <Link to="/results">Search Vans</Link>
+                                </Button>
+                            </>
+                        </Col>
+                    </Row>
+                    {!user ? (
+                        <Row className="d-flex d-flex justify-content-center row-cards">
+                            <Card className="card-style" style={{ width: "8rem", height: "auto" }}>
+                                <Card.Img variant="top" src="./../images/signin.png" />
+                                <Card.Body className="d-flex justify-content-center">
+                                    <Link to="/singup" className="nav-link logo-img">
+                                        <Card.Title>Signup</Card.Title>
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                            <Card className="card-style" style={{ width: "8rem", height: "auto" }}>
+                                <Card.Img variant="top" src="./../images/login.png" />
+                                <Card.Body className="d-flex justify-content-center">
+                                    <Link to="/login" className="nav-link  logo-img">
+                                        <Card.Title>Login</Card.Title>
+                                    </Link>
+                                </Card.Body>
+                            </Card>
+                        </Row>
+                    ) : (
+                        <Row className="d-flex d-flex justify-content-center row-cards">
+                            <Card className="card-style" style={{ width: "8rem", height: "auto" }}>
+                                <Card.Img variant="top" src="./../images/logout.png" />
+                                <Card.Body className="d-flex justify-content-center">
+                                    <Button variant="light" onClick={logOutUser}>
+                                        <Card.Title>logout</Card.Title>
+                                    </Button>
+                                </Card.Body>
+                            </Card>
+                        </Row>
+                    )}
+                </Container>
+            </section>
+        </>
     )
 }
 
