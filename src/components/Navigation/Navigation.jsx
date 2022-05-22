bf3488dd036c189aea4389443f38862a6814dbfd
Arturo Echanique <arturoechanique@gmail.com>
2022-05-22 20:54:17 +0200

    results page redesign

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -76,6 +76,9 @@ const Navigation = () => {
                                 <img className="logo-nav" id="logo" src="./../images/1.png" alt="vanmeup" />
                             </Link>
                         </Navbar.Brand>
+
+
+                        
                         <Navbar.Toggle className="toggle-nav" aria-controls={`offcanvasNavbar-expand-${expand}`} />
                         <Navbar.Offcanvas id={`offcanvasNavbar-expand-${expand}`} aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement="end">
                             <Offcanvas.Header closeButton>
