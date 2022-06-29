93fb2adb816d882da247f8e4ad4e9baf8925ecbc
Arturo Echanique <arturoechanique@gmail.com>
2022-06-29 19:39:26 +0200

    try fix map info view

--- src/components/MapMarker/MapMarker.jsx
+++ src/components/MapMarker/MapMarker.jsx
@@ -1,5 +1,6 @@
 import VanCard from "../VanCard/VanCard"
 import { Marker, InfoWindow } from "@react-google-maps/api"
+import "./MapMarker.css"
 
 const MapMarker = ({ isFavorite, addFavoriteVan, removeFavoriteVan, van, markerIdx, showInfo, setVisibleMarker }) => {
     let iconMarker = new window.google.maps.MarkerImage(
@@ -21,7 +22,10 @@ const MapMarker = ({ isFavorite, addFavoriteVan, removeFavoriteVan, van, markerI
         <Marker onClick={() => setVisibleMarker(showInfo ? -1 : van._id)} position={{ lat: van.location.coordinates[0], lng: van.location.coordinates[1] }} label={label} icon={iconMarker}>
             {showInfo && (
                 <InfoWindow position={{ lat: van.location.coordinates[0], lng: van.location.coordinates[1] }} onCloseClick={() => setVisibleMarker(-1)}>
-                    <VanCard hideDeleteButton={true} isFavorite={isFavorite} addFavoriteVan={addFavoriteVan} removeFavoriteVan={removeFavoriteVan} {...van} />
+                    <div className="infoWindowContainer" style={{ margin: "5%"}}>
+                        <VanCard hideDeleteButton={true} isFavorite={isFavorite} addFavoriteVan={addFavoriteVan} removeFavoriteVan={removeFavoriteVan} {...van} />
+
+                    </div>
                 </InfoWindow>
             )}
         </Marker>
