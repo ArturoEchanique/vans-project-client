da83019b18f1e720b52cfc662e5d18460e8260e2
Arturo Echanique <arturoechanique@gmail.com>
2022-06-29 19:43:41 +0200

    fixing infoview

--- src/components/MapMarker/MapMarker.jsx
+++ src/components/MapMarker/MapMarker.jsx
@@ -22,10 +22,7 @@ const MapMarker = ({ isFavorite, addFavoriteVan, removeFavoriteVan, van, markerI
         <Marker onClick={() => setVisibleMarker(showInfo ? -1 : van._id)} position={{ lat: van.location.coordinates[0], lng: van.location.coordinates[1] }} label={label} icon={iconMarker}>
             {showInfo && (
                 <InfoWindow position={{ lat: van.location.coordinates[0], lng: van.location.coordinates[1] }} onCloseClick={() => setVisibleMarker(-1)}>
-                    <div className="infoWindowContainer" style={{ margin: "5%"}}>
                         <VanCard hideDeleteButton={true} isFavorite={isFavorite} addFavoriteVan={addFavoriteVan} removeFavoriteVan={removeFavoriteVan} {...van} />
-
-                    </div>
                 </InfoWindow>
             )}
         </Marker>
